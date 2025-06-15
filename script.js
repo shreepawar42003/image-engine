64d78f511a038a1aaee0e0f5bd062f571423d561
shreepawar42003 <shreepawar417@gmail.com>
2025-06-15 15:50:54 +0530

    change

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ async function seachImage(params) {
      const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}&per_page=12`;
 
      const response = await fetch(url);
-     const data =  await response.json(); 
+     const data =  await response.json();  
 
      if(page ===1){
         seachresult.innerHTML =" ";
